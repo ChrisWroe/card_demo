a83d20dc9c41a9d1f4df1ec32503bd7dcb662e83
ChrisWroe <ChrisWroe@users.noreply.github.com>
2025-02-09 12:57:44 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
+st.title("🎈 My Tarrot app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Tarrot"
 )
+
+def load_data():
+    data = pd.read_csv("data/cards.csv")
+    return data
+
+data=load_data()
+
+st.subheader('Raw data')
+st.write(data)
